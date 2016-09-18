3a692bf2d5ef73e47b8b5f6ab3b27f0628969d8f
aviad10m <aviad10m@gmail.com>
2016-09-19 00:05:22 +0300

    Added API for Comment in server, with mongo connection

--- server/config/connections.js
+++ server/config/connections.js
@@ -28,9 +28,9 @@ module.exports.connections = {
   * Installed by default.                                                    *
   *                                                                          *
   ***************************************************************************/
-  localDiskDb: {
-    adapter: 'sails-disk'
-  },
+  // localDiskDb: {
+  //   adapter: 'sails-disk'
+  // },
 
   /***************************************************************************
   *                                                                          *
@@ -56,14 +56,13 @@ module.exports.connections = {
   * Run: npm install sails-mongo                                             *
   *                                                                          *
   ***************************************************************************/
-  // someMongodbServer: {
-  //   adapter: 'sails-mongo',
-  //   host: 'localhost',
-  //   port: 27017,
-  //   user: 'username', //optional
-  //   password: 'password', //optional
-  //   database: 'your_mongo_db_name_here' //optional
-  // },
+  // user and password are defined in local.js  
+  mongoDb: {
+    adapter: 'sails-mongo',
+    host: 'ds033116.mlab.com',
+    port: 33116,
+    database: 'react_dev' //optional
+  },
 
   /***************************************************************************
   *                                                                          *
