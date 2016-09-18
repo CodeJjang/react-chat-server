cf1ad4806c8b5f086dc1882f5faf7f074dcaad8d
aviad10m <aviad10m@gmail.com>
2016-09-19 00:34:32 +0300

    Started integrating ajax calls to server

--- client/src/Components/CommentBox/CommentBox.js
+++ client/src/Components/CommentBox/CommentBox.js
@@ -1,13 +1,45 @@
 import React, { Component } from 'react';
+import $ from 'jquery';
 import CommentForm from '../CommentForm';
 import CommentList from '../CommentList';
 
 class CommentBox extends Component {
+	constructor(props) {
+		super( props );
+		this.state = { commentsApiUrl: 'http://localhost:1337/comment' };
+	}
+	componentDidMount() {
+		$.ajax({
+	      url: this.state.commentsApiUrl,
+	      dataType: 'json',
+	      cache: false,
+	      success: function(data) {
+	        this.setState({data: data});
+	      }.bind(this),
+	      error: function(xhr, status, err) {
+	        console.error(this.state.commentsApiUrl, status, err.toString());
+	      }.bind(this)
+	    });
+	}
+	handleCommentSubmit(comment) {
+		$.ajax({
+	      url: this.state.commentsApiUrl,
+	      dataType: 'json',
+	      type: 'POST',
+	      data: comment,
+	      success: function(data) {
+	        this.setState({data: data});
+	      }.bind(this),
+	      error: function(xhr, status, err) {
+	        console.error(this.state.commentsApiUrl, status, err.toString());
+	      }.bind(this)
+	    });
+	}
 	render() {
 		return (
 			<div>
 			<CommentForm />
-			<CommentList />
+			<CommentList onCommentSubmit={ this.handleCommentSumit }/>
 			</div>
 		);
 	}
--- client/src/Components/CommentForm/CommentForm.js
+++ client/src/Components/CommentForm/CommentForm.js
@@ -2,47 +2,55 @@ import React, { Component } from 'react';
 
 class CommentForm extends Component {
 	constructor(props) {
-		super(props);
-		this.state = { author: '', text: ''};
-		this.handleAuthorChange = this.handleAuthorChange.bind(this);
-		this.handleTextChange = this.handleTextChange.bind(this);
+		super( props );
+		this.state = {
+			author: '',
+			text: ''
+		};
+		this.handleAuthorChange = this.handleAuthorChange.bind( this );
+		this.handleTextChange = this.handleTextChange.bind( this );
 	}
 	handleAuthorChange(e) {
-		this.setState({ author: e.target.value });
+		this.setState( {
+			author: e.target.value
+		} );
 	}
 	handleTextChange(e) {
-		this.setState({ text: e.target.value });
+		this.setState( {
+			text: e.target.value
+		} );
 	}
-	handleSubmit() {
-		handleSubmit: function(e) {
-	    e.preventDefault();
-	    var author = this.state.author.trim();
-	    var text = this.state.text.trim();
-	    if (!text || !author) {
-	      return;
-	    }
-	    // TODO: send request to the server
-	    this.setState({author: '', text: ''});
-  }
+	handleSubmit(e) {
+		e.preventDefault();
+		var author = this.state.author.trim();
+		var text = this.state.text.trim();
+		if (!text || !author) {
+			return;
+		}
+		this.props.onCommentSubmit({ author: author, text: text });
+		this.setState( {
+			author: '',
+			text: ''
+		} );
 	}
 	render() {
 		return (
 			<div>
-			<form className="commentForm">
-	        	<input
-	        		type="text"
-	        		value={ this.state.author }
-	        		onChange={ this.handleAuthorChange }
-	        		placeholder="Your name"
-	        		/>
-	        	<input
-	        		type="text"
-	        		value={ this.state.text }
-	        		onChange={ this.handleTextChange }
-	        		placeholder="Say something..."
-	        		/>
-	        	<input type="submit" value="Post" />
-      		</form>
+				<form className='commentForm' onSubmit={ this.handleSubmit }>
+			        <input
+						type='text'
+						value={ this.state.author }
+						onChange={ this.handleAuthorChange }
+						placeholder='Your name'
+					/>
+			        <input
+						type='text'
+						value={ this.state.text }
+						onChange={ this.handleTextChange }
+						placeholder='Say something...'
+					/>
+			        <input type='submit' value='Post' />
+	      		</form>
 			</div>
 		);
 	}
