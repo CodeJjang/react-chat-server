fbcc9a875da74d45e3f86a2db5dac6c47a16f0fd
aviad10m <aviad10m@gmail.com>
2016-09-23 16:08:52 +0300

    Fixed comment headers and body to be below

--- client/src/Components/Comment/Comment.js
+++ client/src/Components/Comment/Comment.js
@@ -5,27 +5,12 @@ import moment from 'moment';
 
 class Comment extends Component {
 	render() {
-		// return (
-		// 	<div className='Comment'>
-		// 		<div className='Comment-title'>
-		// 			<span className='Author-name'>{ this.props.author }</span>
-		// 			<span className='Comment-timestamp'>{ moment(this.props.timestamp).fromNow() }</span>
-		// 		</div>
-		// 		<p className='Comment-text'>{ this.props.text }</p>
-		// 	</div>
-		// 	);
 		return (
 			<Media className='Comment'>
-				<Media.Left>
-					<img width={ 64 }
-						height={ 64 }
-						src='/assets/thumbnail.png'
-						alt='Image' />
-				</Media.Left>
 				<Media.Body>
-					<Media.Heading >
-						<Media.Left className='Author-name'>{this.props.author}</Media.Left>
-						<Media.Right >{moment(this.props.timestamp).fromNow()}</Media.Right>
+					<Media.Heading>
+						<span className='Comment-author'>{this.props.author}</span>
+						<span className='Comment-timestamp'>{moment(this.props.timestamp).fromNow()}</span>
 					</Media.Heading>
 					<Media.Left>
 					<p >
