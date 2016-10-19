312530d566cf8827d742113cd8a28e91b9b12eb9
aviad10m <aviad10m@gmail.com>
2016-10-20 00:19:41 +0300

    Extracted routes to routes.js file

--- client/src/index.js
+++ client/src/index.js
@@ -1,21 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Router, Route, browserHistory, IndexRoute } from 'react-router';
-import App from './App';
-import About from './Components/About';
-import Chat from './Components/Chat';
+import { Router, browserHistory } from 'react-router';
+import routes from './routes';
 import 'bootstrap/dist/css/bootstrap.css';
 import 'bootstrap/dist/css/bootstrap-theme.css';
 import './index.css';
 
 ReactDOM.render(
-  <Router history={browserHistory}>
-    <Route path="/" component={App}>
-    	<IndexRoute component={Chat}/>
-    	<Route path="/chat" component={Chat}/>
-    	<Route path="/room/:id" component={Chat}/>
-    	<Route path="/about" component={About}/>
-    </Route>
-  </Router>,
+  <Router history={browserHistory} routes={routes} />,
   document.getElementById('root')
 );
---
+++ client/src/routes.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Route, IndexRoute } from 'react-router';
+import App from './App';
+import About from './Components/About';
+import Chat from './Components/Chat';
+
+export default (
+	<Route path="/" component={App}>
+    	<IndexRoute component={Chat}/>
+    	<Route path="/chat" component={Chat}/>
+    	<Route path="/room/:id" component={Chat}/>
+    	<Route path="/about" component={About}/>
+    </Route>
+);
