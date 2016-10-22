329b123715940e6d457b3310a6fa90443cae527a
aviad10m <aviad10m@gmail.com>
2016-10-22 20:28:52 +0300

    Added redux-thunk library

--- client/src/Store/ConfigureStore.js
+++ client/src/Store/ConfigureStore.js
@@ -1,11 +1,12 @@
 import { createStore, applyMiddleware } from 'redux';
 import RootReducer from '../Reducers';
 import reduxImmutableStateInvariant from 'redux-immutable-state-invariant';
+import thunk from 'redux-thunk';
 
 export default function(initialState) {
 	return createStore (
 			RootReducer,
 			initialState,
-			applyMiddleware(reduxImmutableStateInvariant())
+			applyMiddleware(thunk, reduxImmutableStateInvariant())
 		);
 }
